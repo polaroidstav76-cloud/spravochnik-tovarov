ffdd0b344d45d596335190f46231d639db723b56
polaroidstav76-cloud <polaroid.stav76@gmail.com>
2026-09-19 15:32:42 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Вставьте сюда данные проекта Supabase.
+// Supabase Dashboard -> Project Settings -> API
+const SUPABASE_URL = 'https://tmxluzvymwldtxbaiekn.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_seHlOPu_cbFZOCmqG9UCzQ_9411endl';
